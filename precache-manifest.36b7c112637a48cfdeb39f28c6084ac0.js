176b00cf1466c77e76a82eaaf026073afe4dddb8
Suresh K L <suresh.1591@gmail.com>
2019-07-13 14:09:16 +0530

    Updates

---
+++ precache-manifest.36b7c112637a48cfdeb39f28c6084ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab242d89257609aee18a5c676921e3f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5a19a62f34380c584d48",
+    "url": "/monsters/static/css/main.dfb2a969.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5a19a62f34380c584d48",
+    "url": "/monsters/static/js/main.19d8fd34.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
